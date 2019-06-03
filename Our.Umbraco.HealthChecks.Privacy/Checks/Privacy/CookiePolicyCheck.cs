18c7090280130fec2996975fb4e3ee6a01efe454
Alan Mac Kenna <alan.mackenna@serveit.ie>
2019-06-03 14:45:20 +0100

    Initial Commit: Privacy Policy & Cookie Policy Health Check

---
+++ Our.Umbraco.HealthChecks.Privacy/Checks/Privacy/CookiePolicyCheck.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Services;
+using Umbraco.Web.HealthCheck;
+using Umbraco.Web.HealthCheck.Checks.Config;
+
+namespace Our.Umbraco.HealthChecks.Privacy.Checks.Privacy
+{
+    [HealthCheck("4A616B92-AC7C-4CA0-9F77-08E8543AEC8E", "Cookie Policy Check - (from Our.Umbraco.HealthChecks.Privacy)",
+        Description = "Check to ensure a Cookie Policy is selected",
+        Group = "Privacy")]
+    public class CookiePolicyCheck : AbstractConfigCheck
+    {
+        public CookiePolicyCheck(ILocalizedTextService textService)
+            : base(textService)
+        { }
+
+        public override string FilePath => "~/Web.config";
+
+        public override string XPath => "/configuration/appSettings/add[@key='Our.Umbraco.HealthChecks.Privacy.CookiePolicyUDI']/@value";
+
+        public override ValueComparisonType ValueComparisonType => ValueComparisonType.ShouldNotEqual;
+
+        public override ProvidedValuePropertyType ProvidedValuePropertyType => ProvidedValuePropertyType.ContentPicker;
+
+        public override IEnumerable<AcceptableConfiguration> Values => new List<AcceptableConfiguration>
+        {
+            new AcceptableConfiguration { IsRecommended = false, Value = string.Empty }
+        };
+
+        // todo: handle language files
+        // public override string CheckSuccessMessage => TextService.Localize("Our.Umbraco.HealthChecks.Privacy/cookiePolicyConfigSuccess");
+        public override string CheckSuccessMessage => "A Cookie Policy has been selected.";
+
+        // todo: handle language files
+        // public override string CheckErrorMessage => TextService.Localize("Our.Umbraco.HealthChecks.Privacy/cookiePolicyError");
+        public override string CheckErrorMessage => "A Cookie Policy needs to be selected.";
+
+        // todo: handle language files
+        // public override string RectifySuccessMessage => TextService.Localize("Our.Umbraco.HealthChecks.Privacy/cookiePolicyRectifySuccess");
+        public override string RectifySuccessMessage => "You have successfully selected a Cookie Policy.";
+    }
+}
+
---
+++ Our.Umbraco.HealthChecks.Privacy/Checks/Privacy/PrivacyPolicyCheck.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core.Services;
+using Umbraco.Web.HealthCheck;
+using Umbraco.Web.HealthCheck.Checks.Config;
+
+namespace Our.Umbraco.HealthChecks.Privacy.Checks.Privacy
+{
+    [HealthCheck("4E7B05BE-F77D-4B8A-8E77-F0FB96E0BDF8", "Privacy Policy Check - (from Our.Umbraco.HealthChecks.Privacy)",
+        Description = "Check to ensure a Privacy Policy is selected.",
+        Group = "Privacy")]
+    public class PrivacyPolicyCheck : AbstractConfigCheck
+    {
+        public PrivacyPolicyCheck(ILocalizedTextService textService)
+            : base(textService)
+        { }
+
+        public override string FilePath => "~/Web.config";
+
+        public override string XPath => "/configuration/appSettings/add[@key='Our.Umbraco.HealthChecks.Privacy.PrivacyPolicyUDI']/@value";
+
+        public override ValueComparisonType ValueComparisonType => ValueComparisonType.ShouldNotEqual;
+
+        public override ProvidedValuePropertyType ProvidedValuePropertyType => ProvidedValuePropertyType.ContentPicker;
+
+        public override IEnumerable<AcceptableConfiguration> Values => new List<AcceptableConfiguration>
+        {
+            new AcceptableConfiguration { IsRecommended = false, Value = string.Empty }
+        };
+
+        // todo: handle language files
+        // public override string CheckSuccessMessage => TextService.Localize("Our.Umbraco.HealthChecks.Privacy/privacyPolicyConfigSuccess");
+        public override string CheckSuccessMessage => "A Privacy Policy has been selected.";
+
+        // todo: handle language files
+        // public override string CheckErrorMessage => TextService.Localize("Our.Umbraco.HealthChecks.Privacy/privacyPolicyError");
+        public override string CheckErrorMessage => "A Privacy Policy needs to be selected.";
+
+        // todo: handle language files
+        // public override string RectifySuccessMessage => TextService.Localize("Our.Umbraco.HealthChecks.Privacy/privacyPolicyRectifySuccess");
+        public override string RectifySuccessMessage => "You have successfully selected a Privacy Policy.";
+    }
+}
